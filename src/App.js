5b002f3d9eaea31fde6c7194e23a8dacc208f8c0
Murat <mrtgms3561@gmail.com>
2021-07-12 14:04:11 +0300

    Get API Key

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+const api = {
+  key: "7a1165303af2fa25a56251e528980a33"
+}
+
+const  App = () => {
+  return (
+    <div className="App">
+     Weather app
+    </div>
+  );
+}
+
+export default App;
